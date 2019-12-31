5f850a2d7ea426304e6f0d03fd2c54eae3258ee6
chetan0541 <33390795+chetan0541@users.noreply.github.com>
2019-12-31 16:07:49 +0100

    Update SimpleRegression.py
    A pretty good model

--- SimpleRegression.py
+++ SimpleRegression.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np 
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression 
+import matplotlib.pyplot as plt 
 
 data = pd.read_csv("Datasets/Salary_Data.csv")
 
@@ -18,3 +19,21 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 model = LinearRegression()
 model.fit(X_train, y_train)
+
+
+y_pred = model.predict(X_test)
+
+# print(y_pred)
+# plt.scatter(X_train, y_train, color = 'red')
+# plt.plot(X_train, model.predict(X_train), color= 'black')
+# plt.title('Salary and Experience Relationship(Training Set)')
+# plt.xlabel('Years of Experience')
+# plt.ylabel('Salary')
+# plt.show()
+
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_test, y_pred, color= 'black')
+plt.title('Salary and Experience Relationship(Test Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
