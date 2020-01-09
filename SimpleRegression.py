488abf4126a0dfc67651abbf25b80770a0c5b9f7
chetan0541 <33390795+chetan0541@users.noreply.github.com>
2020-01-09 17:32:40 +0100

    Update SimpleRegression.py

--- SimpleRegression.py
+++ SimpleRegression.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression 
 import matplotlib.pyplot as plt 
+import statsmodels.api as sm
+from yellowbrick.regressor import ResidualsPlot
 
 data = pd.read_csv("Datasets/Salary_Data.csv")
 
@@ -21,9 +23,10 @@ model = LinearRegression()
 model.fit(X_train, y_train)
 
 
-y_pred = model.predict(X_test)
+# y_pred = model.predict(X_test)
 
 # print(y_pred)
+#Plotting Training values on a scatter plot
 # plt.scatter(X_train, y_train, color = 'red')
 # plt.plot(X_train, model.predict(X_train), color= 'black')
 # plt.title('Salary and Experience Relationship(Training Set)')
@@ -31,9 +34,25 @@ y_pred = model.predict(X_test)
 # plt.ylabel('Salary')
 # plt.show()
 
-plt.scatter(X_test, y_test, color = 'red')
-plt.plot(X_test, y_pred, color= 'black')
-plt.title('Salary and Experience Relationship(Test Set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
-plt.show()
+#Checking Homoscedasticity
+# hom = ResidualsPlot(model)
+# hom.fit(X_train, y_train)
+# hom.score(X_test, y_test)
+# hom.poof() 
+
+#Checking Normal Distribution of error terms
+# model = sm.OLS(y_train, X_train).fit()
+# res = model.resid
+# plot = sm.qqplot(res, fit=True, line='r')
+# plt.show()
+
+#Checking autocorrelation using Durbin-Watson
+# model = sm.OLS(y_train, X_train).fit()
+# print(model.summary())
+
+# plt.scatter(X_test, y_test, color = 'red')
+# plt.plot(X_test, y_pred, color= 'black')
+# plt.title('Salary and Experience Relationship(Test Set)')
+# plt.xlabel('Years of Experience')
+# plt.ylabel('Salary')
+# plt.show()
